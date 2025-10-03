6d808993910040173bc1669478ce0ce74b199141
ssm <tjtkdan1072@gmail.com>
2025-10-03 22:24:34 +0900

    ArgumentResolver 를 활ì활용하여 @Login 생성후 ì 적용

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -76,9 +77,22 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     // 이미 로그인 된 사용자를 찾을 때는 다음과 같이 사용하면 좋음 @SessionAttribute
     public String loginHomeV4(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String loginHomeV4ArgumentResolver(@Login Member loginMember, Model model) {
 
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
